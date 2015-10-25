7427a43de43dba456219fdaf7ec7e953565f3421
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2015-10-25 18:20:16 +1300

    homepage basic layout

--- index.php
+++ index.php
@@ -12,5 +12,7 @@
 	<?php include 'php/homepage/hp-intro.php' ?>
 	<?php include 'php/homepage/hp-new-member.php' ?>
 	<?php include 'php/homepage/hp-join.php' ?>
+	<?php include 'php/homepage/hp-current.php' ?>
+	<?php include 'php/footer.php' ?>
 </body>
 </html>
--- php/homepage/hp-new-member.php
+++ php/homepage/hp-new-member.php
@@ -1,19 +1,20 @@
-<div class="container">
-	<div class="row">
-		<div class="col-sm-12">
-			<h2>New Member</h2>
+<section class="section-spacing section-dark">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2>New Member</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-2">
+				<img class="img-responsive hp-nm-image" src="images/mm.png" alt="">
+			</div>
+			<div class="col-sm-10">
+				<p>The Sarah Bichan Guild would like to welcome thier newest member Sarah Bichan</p>
+				<p>Here is a blurb that container interesting information about Sarah Bichan
+	Sarah is recently married to Tyler Bichan</p>
+				<p>Follow Sarahs Wedding Day twitter story at #Hashtag</p>
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-sm-2 hp-nm-image">
-			<img class="img-responsive" src="images/mm.png" alt="">
-		</div>
-		<div class="col-sm-10">
-			<p>The Sarah Bichan Guild would like to welcome thier newest member Sarah Bichan</p>
-			<p>Here is a blurb that container interesting information about Sarah Bichan
-Sarah is recently married to Tyler Bichan</p>
-			<p>Follow Sarahs Wedding Day twitter story at #Hashtag</p>
-		</div>
-	</div>
-
-</div>
+</section>
