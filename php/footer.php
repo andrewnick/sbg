e05cd8a33bdfdee8f5d9257556296fcfb1b46dab
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2015-10-25 18:20:20 +1300

    homepage basic layout

---
+++ php/footer.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+	<div class="container footer-lines">
+		<div class="row">
+			<div class="col-sm-6 footer-detail">
+				<p><?php echo "Copyright &copy; " . date("Y") . " Sarah Bichan Guild" ?></p>
+			</div>
+			<div class="col-sm-6 footer-detail text-right">
+				<?php include 'nav.php' ?>
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ php/homepage/hp-current.php
@@ -0,0 +1,25 @@
+<section class="section-spacing section-dark">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2>Current Members</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="profile">
+					<img class="img-responsive profile-image" src="images/sarah-img.png" alt="">
+					<p class="profile-name">Sarah Bichan</p>
+					<p class="profile-bio">Blurb about Sarah</p>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="profile">
+					<img class="img-responsive profile-image" src="images/sarah-img.png" alt="">
+					<p class="profile-name">Sarah Bichan</p>
+					<p class="profile-bio">Blurb about Sarah</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
