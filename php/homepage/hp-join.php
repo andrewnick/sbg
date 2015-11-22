6d457da7817fd12ee67d1b32035a245437ca53f3
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2015-11-22 16:22:00 +1300

    fix link

--- php/homepage/hp-join.php
+++ php/homepage/hp-join.php
@@ -7,12 +7,26 @@
 		</div>
 		<div class="row">
 			<div class="col-sm-12">
-				<p class="hp-join-text">More the Merrier! If you want to go to our sign up page. Here is some more text for the blurb</p>
+				<p class="hp-join-text"><p>If you feel like you meet the requirements of the Sarah Bichan Guild and would like to apply for a membership, click here.</p>
+
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-sm-6">
-				<a class="btn btn-default btn-lite-gold btn-join" href="/sbg/join">Join the Guild</a>
+				<a class="btn btn-default btn-lite-gold btn-join" href="/join">Join the Guild</a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12">
+						<p>Please keep in mind, acceptance to the Guild is at the discretion of the Membership Panel. Their commitment to upholding the high standards of the Sarah Bichan Guild is unprecedented. It is not uncommon for applicants to be rejected several times before they meet the entry criteria.</p>
+				<p>Rejected applicants include:</p>
+				<ul>
+					<li>Sarah Jessica Parker</li>
+					<li>Sarah Michelle Gellar</li>
+					<li>Sarah Palin</li>
+					<li>Sarah Ferguson</li>
+					<li>Cam Bichan</li>
+				</ul>
 			</div>
 		</div>
 	</div>
