f27bcce7e58a54497199a9a98708ad981003bb90
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2015-11-22 15:39:33 +1300

    change new member blurb

--- php/homepage/hp-new-member.php
+++ php/homepage/hp-new-member.php
@@ -10,10 +10,14 @@
 				<img class="img-responsive hp-nm-image" src="images/mm.png" alt="">
 			</div>
 			<div class="col-sm-10">
-				<p>The Sarah Bichan Guild would like to welcome thier newest member Sarah Bichan</p>
-				<p>Here is a blurb that container interesting information about Sarah Bichan
-	Sarah is recently married to Tyler Bichan</p>
-				<p>Follow Sarahs Wedding Day twitter story at #Hashtag</p>
+			<p>The Sarah Bichan Guild would like to warmly welcome their newest member, Sarah Bichan.</p>
+
+			<p>Sarah Bichan comes to us as a beautiful and talented young woman, with her mind set on changing the world. As a second year teacher at Gladstone Primary School, Sarah Bichan is renown for her wise and trustworthy ways. It was our pleasure to accept Sarah’s application to join the Guild.</p>
+
+			<p>It speaks highly of the newest Sarah Bichan that she was so eager to join our ranks that she was even prepared to marry Tyler Bichan. We are most grateful for her sacrifice.</p>
+
+			<p>The WoolleyBichan wedding was a highlight on the Guild’s 2015 social calendar. Follow the story of her wedding day at #woolleywedsbichan on instagram. </p>
+
 				<a class="twitter-timeline" href="https://twitter.com/4Nicho1as" data-widget-id="660721837618270209">Tweets by @4Nicho1as</a>
 
 				<div id="instafeed"></div>
