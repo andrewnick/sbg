c0c9cf84c80615f8df6c43d390e584b41026bf6e
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2015-11-22 17:58:46 +1300

    add new content

---
+++ php/homepage/hp-slogan.php
@@ -0,0 +1,7 @@
+<div class="container hp-intro">
+	<div class="row">
+		<div class="col-sm-12">
+		<p class="slogan-text">"We firmly believe in the transforming power of Sarah Bichans and do all we can to empower each individual to be a superior members of her, or his, community."</p>
+		</div>
+	</div>
+</div>
