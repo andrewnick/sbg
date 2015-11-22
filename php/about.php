0fd81ca3fbf20667028c86080bed3ed327107c9f
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2015-11-22 15:17:10 +1300

    setup for production site

---
+++ php/about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php include 'head.php' ?>
+<body class="bg">
+	<?php include 'header.php' ?>
+	<section class="section-spacing">
+		<div class="container">
+			<div class="row join-spacer">
+				<div class="col-sm-6">
+					<h2>About</h2>
+				</div>
+			</div>
+			<div class="row join-spacer">
+				<p>The founding member Sarah Bichan was brought up in West auckland</p>
+			</div>
+		</div>
+	</section>	
+
+	<?php include 'footer.php' ?>
+</body>
+</html>
---
+++ php/head.php
@@ -0,0 +1,13 @@
+<head>
+	<meta charset="UTF-8">
+	<title>Sarah Bichan Guild</title>
+	
+	<script src="http://localhost:8080/assets/css.bundle.js"></script>
+	<script src="http://localhost:8080/assets/js.bundle.js"></script>
+	
+	<script src="assets/css.bundle.js"></script>
+	<script src="assets/js.bundle.js"></script>
+	<script type="text/javascript" src="js/instafeed.min.js"></script>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
