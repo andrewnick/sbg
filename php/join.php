c2b41b5ad4ffe9b2ccc53ad049b4ea6efb024696
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2015-11-22 18:05:40 +1300

    fix margins on join

--- php/join.php
+++ php/join.php
@@ -11,53 +11,50 @@
 					<p class="text-max-width">If you want to join The Guild simply fill out this form with your details and you will receive an email about the next step to join.</p>
 			</div>
 		</div>
-				</div>
+		<div class="row">
+			<div class="col-sm-6">
+
+				<h3> Requirements </h3>
+				<ol class="join-list">
+					<li>Your name must be Sarah Bichan.</li>
+					<li>
+						<p>Acceptance to the Guild is at the discretion of the Membership Panel. Their commitment to upholding the high standards of the Sarah Bichan Guild is unprecedented. It is not uncommon for applicants to be rejected several times before they meet the entry criteria.</p>
+						<p>Rejected applicants include:</p>
+						<ul>
+							<li>Sarah Jessica Parker</li>
+							<li>Sarah Michelle Gellar</li>
+							<li>Sarah Palin</li>
+							<li>Sarah Ferguson</li>
+							<li>Cam Bichan</li>
+						</ul>
+					</li>
+				</ol>
+
+
+				<form method="POST" action="" class="join-form">
+				  <div class="form-group">
+				    <label for="name">Name</label>
+				    <input type="text" name="name" class="form-control" id="name" placeholder="Name">
+				  </div>
+				  <div class="form-group">
+				    <label for="name">Address</label>
+				    <input type="text" name="address" class="form-control" id="address" placeholder="Address">
+				  </div>
+				  <button type="submit" class="btn btn-default btn-lite-gold">Join</button>
+				</form>
+
+
 			</div>
-			<div class="row">
-				<div class="col-sm-6">
-
-					<h3> Requirements </h3>
-					<ol class="join-list">
-						<li>Your name must be Sarah Bichan.</li>
-						<li>
-							<p>Acceptance to the Guild is at the discretion of the Membership Panel. Their commitment to upholding the high standards of the Sarah Bichan Guild is unprecedented. It is not uncommon for applicants to be rejected several times before they meet the entry criteria.</p>
-							<p>Rejected applicants include:</p>
-							<ul>
-								<li>Sarah Jessica Parker</li>
-								<li>Sarah Michelle Gellar</li>
-								<li>Sarah Palin</li>
-								<li>Sarah Ferguson</li>
-								<li>Cam Bichan</li>
-							</ul>
-						</li>
-					</ol>
-
-
-					<form method="POST" action="" class="join-form">
-					  <div class="form-group">
-					    <label for="name">Name</label>
-					    <input type="text" name="name" class="form-control" id="name" placeholder="Name">
-					  </div>
-					  <div class="form-group">
-					    <label for="name">Address</label>
-					    <input type="text" name="address" class="form-control" id="address" placeholder="Address">
-					  </div>
-					  <button type="submit" class="btn btn-default btn-lite-gold">Join</button>
-					</form>
-
-
+			<div class="col-sm-6">
+				<div class="join-sidebox">
+					<h3>Your name isn't Sarah Bichan?</h3>
+					<a href="http://www.dia.govt.nz/Services-Births-Deaths-and-Marriages-Changing-a-Name?OpenDocument#three" class="btn btn-default btn-lite-gold" target="_blank">Change your name</a>
 				</div>
-				<div class="col-sm-6">
-					<div class="join-sidebox">
-						<h3>Your name isn't Sarah Bichan?</h3>
-						<a href="http://www.dia.govt.nz/Services-Births-Deaths-and-Marriages-Changing-a-Name?OpenDocument#three" class="btn btn-default btn-lite-gold" target="_blank">Change your name</a>
-					</div>
-					
-					<div class="join-sidebox">
-						<h3>First Name Sarah But Last Name Not Bichan?</h3>
-						<p> Apply to marry Cam Bichan </p>
-						<a href="https://www.facebook.com/cbichan" class="btn btn-default btn-lite-gold" target="_blank"	>View Cam's profile</a>
-					</div>
+				
+				<div class="join-sidebox">
+					<h3>First Name Sarah But Last Name Not Bichan?</h3>
+					<p> Apply to marry Cam Bichan </p>
+					<a href="https://www.facebook.com/cbichan" class="btn btn-default btn-lite-gold" target="_blank"	>View Cam's profile</a>
 				</div>
 			</div>
 		</div>
