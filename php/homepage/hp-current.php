ba4167c73d2ff82ded25b6142be3eee69315dc08
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2015-11-22 18:01:08 +1300

    fix up paragraphs and styles

--- php/homepage/hp-current.php
+++ php/homepage/hp-current.php
@@ -10,20 +10,20 @@
 				<div class="profile">
 					<img class="img-responsive profile-image" src="images/SB1.png" alt="">
 					<p class="profile-name">Sarah Bichan</p>
-					<p class="profile-bio">Sarah Bichan is our longest serving member, joining the guild in the late 1980s. Since becoming a Sarah Bichan, she has taken the world by storm and laid the groundwork for the Sarah Bichans to come. 
+					<p class="profile-bio">Sarah Bichan is our longest serving member, joining the guild in the late 1980s. Since becoming a Sarah Bichan, she has taken the world by storm and laid the groundwork for the Sarah Bichans to come. </p>
 
-Known respectfully as ‘Sarah Bichan The First’ by the other Guild members, Sarah’s advice and guidance is often sought after by our newer members.
+	<p>Known respectfully as ‘Sarah Bichan The First’ by the other Guild members, Sarah’s advice and guidance is often sought after by our newer members.</p>
 
-Sarah Bichan is the Founding Director of The Sarah Bichan Guild, the Chairsarah of the Membership Panel, Chief Photo-Shopper, and the vice-Chair of the Events Committee.</p>
+<p>Sarah Bichan is the Founding Director of The Sarah Bichan Guild, the Chairsarah of the Membership Panel, Chief Photo-Shopper, and the vice-Chair of the Events Committee.</p>
 				</div>
 			</div>
 			<div class="col-sm-6">
 				<div class="profile">
 					<img class="img-responsive profile-image" src="images/SB2.png" alt="">
 					<p class="profile-name">Sarah Bichan</p>
-					<p class="profile-bio">Sarah Bichan is a reasonably recent addition to The Sarah Bichan Guild, joining in June via an arranged marriage to James Bichan.
+					<p class="profile-bio">Sarah Bichan is a reasonably recent addition to The Sarah Bichan Guild, joining in June via an arranged marriage to James Bichan.</p>
 
-Although her initial transition into Sarah Bichanhood has not been straightforward (the mispronunciation of ‘Bichan’ can, at times, be discouraging,) Sarah has shown the trademark resilience common to Sarah Bichans world wide. 
+<p>Although her initial transition into Sarah Bichanhood has not been straightforward (the mispronunciation of ‘Bichan’ can, at times, be discouraging,) Sarah has shown the trademark resilience common to Sarah Bichans world wide. </p>
 
 Sarah is known to Guild members as being the token fit one and is fondly called ‘Sarah Bichan the Second.’ Although the wider Bichan public struggle with her non-hate of cats,Sarah has won them over with her good looks and charm.</p>
 				</div>
