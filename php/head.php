1a78c12916bf79f34ea6e731d113f9b5e0fcca5f
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2015-12-07 22:16:02 +1300

    fix up footer

--- php/head.php
+++ php/head.php
@@ -2,11 +2,11 @@
 	<meta charset="UTF-8">
 	<title>Sarah Bichan Guild</title>
 	
-	<!-- <script src="http://localhost:8080/assets/css.bundle.js"></script> -->
-	<!-- <script src="http://localhost:8080/assets/js.bundle.js"></script> -->
+	<script src="http://localhost:8080/assets/css.bundle.js"></script>
+	<script src="http://localhost:8080/assets/js.bundle.js"></script>
 	
-	<script src="assets/css.bundle.js"></script>
-	<script src="assets/js.bundle.js"></script>
+	<!-- <script src="assets/css.bundle.js"></script> -->
+	<!-- <script src="assets/js.bundle.js"></script> -->
 	<script src="https://use.typekit.net/euo2kyh.js"></script>
 	<script>try{Typekit.load({ async: true });}catch(e){}</script>
 
