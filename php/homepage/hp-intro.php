77253d37c7356fa85bfa349859aee642ddf21499
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2015-12-08 21:31:42 +1300

    remove rows from full width column

--- php/homepage/hp-intro.php
+++ php/homepage/hp-intro.php
@@ -1,8 +1,8 @@
-<div class="container hp-intro">
-	<div class="row">
+<section class="section-spacing">
+	<div class="container hp-intro">
 		<div class="col-sm-12">
-		<p class="intro-text">Welcome to the Sarah Bichan Guild. </p> 
-		<p class="intro-text intro-text-middle">We are an elite gathering of select Sarah Bichans who meet regularly to celebrate all that sets us apart. The Guild specialises in connecting Sarah Bichans and optimising their unique traits, for the benefit of our greater society and nation.</p>
+			<p class="intro-text">Welcome to the Sarah Bichan Guild. </p> 
+			<p class="intro-text intro-text-middle">We are an elite gathering of select Sarah Bichans who meet regularly to celebrate all that sets us apart. The Guild specialises in connecting Sarah Bichans and optimising their unique traits, for the benefit of our greater society and nation.</p>
 		</div>
 	</div>
-</div>
+</section>
--- php/homepage/hp-slogan.php
+++ php/homepage/hp-slogan.php
@@ -1,7 +1,7 @@
-<div class="container hp-slogan">
-	<div class="row">
-		<div class="col-sm-12">
-		<p class="slogan-text">"We firmly believe in the transforming power of Sarah Bichans and do all we can to empower each individual to be a superior members of her, or his, community."</p>
+<section class="section-spacing">
+	<div class="container hp-slogan">
+		<div class="col-md-12">
+			<p class="slogan-text">"We firmly believe in the transforming power of Sarah Bichans and do all we can to empower each individual to be a superior members of her, or his, community."</p>
 		</div>
 	</div>
-</div>
+</section>
