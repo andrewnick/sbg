cd62a377e93a14472ec08039ced7d79132501408
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2015-12-11 08:40:32 +1300

    add text

--- php/events.php
+++ php/events.php
@@ -23,10 +23,12 @@
 				<div class="col-sm-4">
 					<img src="images/bichan-wedding.jpg" alt="" class="img img-responsive">
 				</div>
-<!-- 				<div class="col-sm-8">
-					<h3>Annual Sarah Bichan Guild Picnic</h3>
-					<p>This year’s annual picnic outing was held at Stonyridge Vineyard on Waiheke Island. It was attended by Sarah Bichan, Sarah Bichan and the future Sarah Bichan. We enjoyed a delicate morning tea and played with Vortex Megahowlers. A great time was had by all. We look forward to our 2016 picnic with great anticipation.</p>
-				</div> -->
+				<div class="col-sm-8">
+					<h3>Sarah Bichan’s Wedding</h3>
+					<p>A big event on the Auckland social scene, the wedding of Sarah Bichan the Second was referred to by the New Zealand Herald as “prestigious and chic.” In true Sarah Bichan fashion, the bride was pulchritudinous and was described by the Bride & Groom magazine as “an example to us all of the perfect bride.” The food at the Kumeu venue was outstanding, and as the New Zealand-wide food magazine Dish stated, “this was everything I expected of a Sarah Bichan wedding, and more.” It goes without saying that the happy couple are thrilled at the addition of a Sarah Bichan to their relationship, and as they quoted in an exclusive interview with Seven Sharp, “this has added a whole new level of meaning to life that we never knew existed.” ‘It is fair to say,’ mentions the Woman’s Day Sarah Bichan Special Edition Issue, ‘that the expanding empire of Sarah Bichans in this country is something to cherish. Their astounding combination of beauty, brains and charm makes them the perfect triple threat for all young daughters to emulate.”</p>
+
+					<p>We congratulate Sarah Bichan on her entry into the Guild and honour James Bichan for being chosen by the Guild to marry Sarah and make a true Bichan out of her.</p>
+				</div>
 			</div>
 		</div>
 	</section>	
